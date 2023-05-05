b3e083b7c4823173b07826161da0318a52d16ae3
SaiVaniPatil <patil.saivani@gmail.com>
2023-05-06 00:06:51 +0200

    distinct and sorted

--- FunctionalProgramming/src/programming/FP02Functional.java
+++ FunctionalProgramming/src/programming/FP02Functional.java
@@ -9,19 +9,59 @@ public class FP02Functional{
 
         List<Integer> numbers =  List.of(12,9,13,4,6,4,12,15);
         
-        int sum = addNumbersinList(numbers);
+        // int sum = addNumbersinList(numbers);
 
-        System.out.println(sum);
+        // System.out.println(sum);
 
-        int sum2 = addNumbersinListLamda(numbers);
+        // int sum2 = addNumbersinListLamda(numbers);
 
-        System.out.println(sum2);
+        // System.out.println(sum2);
+
+        // int sum3 = addNumbersinListLamda(numbers);
+
+        // System.out.println(sum3);
+
+        //exercises
+        //  System.out.println("sumOfSquares =>"+ sumOfSquares(numbers));
+        //  System.out.println("sumOfCubes =>"+ sumOfCubes(numbers));
+        //  System.out.println("sumOfOdds =>"+ sumOfOdds(numbers));
+        //  System.out.println("distinctNumbers =>");
+
+        //   distinctNumbers(numbers);
+
+          System.out.println("sortednNumbers =>");
+
+          sortednNumbers(numbers);
 
     }
 
-    public static int sum(int a, int b)
+    private static void sortednNumbers(List<Integer> numbers) {
+
+      numbers.stream().sorted().forEach(System.out::println);
+    }
+
+    private static void distinctNumbers(List<Integer> numbers) {
+       numbers.stream().distinct().forEach(System.out::println);;
+    }
+
+    private static int sumOfOdds(List<Integer> numbers) {
+      return numbers.stream().filter(num -> num%2!=0).reduce(0,(num1,num2)->num1+num2);
+    }
+
+    private static int sumOfCubes(List<Integer> numbers) {
+      return numbers.stream().map(num -> num*num*num).reduce(0,(num1,num2)->num1+num2); 
+    }
+
+    private static int sumOfSquares(List<Integer> numbers) {
+
+      return numbers.stream().map(num -> num*num).reduce(0,(num1,num2)->num1+num2);
+    }
+
+    public static int sum(int aggregatesum, int nextnumber)
     {
-      return a+b;
+
+      System.out.println(aggregatesum + " " + nextnumber);
+      return aggregatesum+nextnumber;
     }
 
     private static int addNumbersinList(List<Integer> numbers) {
@@ -36,6 +76,17 @@ public class FP02Functional{
       //combine all to single result
       
       return numbers.stream().reduce(0,(num1,num2)->num1+num2);
+
+    
     }
+
+    private static int addNumbersinListmethodReference(List<Integer> numbers) {
+      
+      //combine all to single result
+      
+     
+      return numbers.stream().reduce(0,Integer::sum);
+    }
+
 
 }
