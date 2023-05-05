5a3635428618d7b60c0527f38ed84526761c65bd
SaiVaniPatil <patil.saivani@gmail.com>
2023-05-05 23:11:20 +0200

    functional changes 1

--- FunctionalProgramming/src/programming/FP01Functional.java
+++ FunctionalProgramming/src/programming/FP01Functional.java
@@ -8,6 +8,22 @@ public class FP01Functional {
     public static void main (String[] args)
     {
         printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+   
+        printEvenNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
+    }
+
+    private static boolean isEven(int number)
+    {
+        return number%2==0;
+
+    }
+
+    private static void printEvenNumbersInListStructured(List<Integer> numberList) {
+
+        System.out.println("Print even numbers:");
+
+        numberList.stream().filter(FP01Functional::isEven).forEach(System.out::println);
+
     }
 
     private static void print(int number)
@@ -19,8 +35,10 @@ public class FP01Functional {
 
         //What to do? =>  method reference on static method, defining behaviour
 
-        numberList.stream().forEach(FP01Functional::print);
+       // numberList.stream().forEach(FP01Functional::print); is same as below
 
+
+        numberList.stream().forEach(System.out::println);
 
 
             // for(Integer i : numberList)
--- FunctionalProgramming/src/programming/FP01Structured.java
+++ FunctionalProgramming/src/programming/FP01Structured.java
@@ -7,7 +7,19 @@ public class FP01Structured{
     public static void main (String[] args)
     {
         printAllNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
+        printEvenNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
     }
+    private static void printEvenNumbersInListStructured(List<Integer> numberList) {
+
+
+        System.out.println("Print even numbers:");
+        for(Integer i : numberList)
+        {
+            if(i%2==0)
+            System.out.println(i);
+        }
+    }
+    
 
     private static void printAllNumbersInListStructured(List<Integer> numberList) {
 
